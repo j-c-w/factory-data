b2ec73e9a6b8559c7183529d414984eb28fe6aaa
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-07-22 17:05:33 +0600

    added a small wrapper class around SearchAction

--- src/main/scala/query/SearchQuery.scala
+++ src/main/scala/query/SearchQuery.scala
@@ -4,12 +4,18 @@ import main.scala.datatypes._
 
 /*
  * Created by Jackson Woodruff on 22/07/2014 
- * 
+ *
+ *
+ * A small class that hides the monstrous complexity
+ * of the Searches and SearchAction classes
  */
 
-class SearchQuery(actions: List[SearchAction]) {
-  this = new SearchQuery(Nil)
+class SearchQuery(actions: Searches) {
+  def this() = this(Nil)
 
-  def add(action: SearchAction, combinator: (SearchAction, SearchAction) => SearchAction) =
-    new SearchQuery()
+  def add(action: SearchAction[Any, Any], combinator: (SearchAction, SearchAction) => SearchAction) =
+    new SearchQuery(actions.add(action))
+
+  def search(list: List[LineListObject]): List[LineListObject] =
+    actions.search(list)
 }
