2e4a8efcfe968cee2301b6c10743c715030ea34b
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-07-24 22:05:55 +0600

    removed accidentally inserted trait in FilterBuilder

--- src/main/scala/query/FilterBuilder.scala
+++ src/main/scala/query/FilterBuilder.scala
@@ -15,24 +15,6 @@ class FilterBuilder[T <: DataType[T]](val f: T => Boolean) {
 
   def and(f2: T => Boolean) =
     add(f2, _ && _)
-  /*
-   * Created by Jackson Woodruff on 24/07/2014
-   *
-   * This trait is here for historical reference, not actual use.
-   *
-   * The real problem with this trying to use this trait is that
-   * the anonymous functions that I have been using to shift
-   * data around do not work at all well with this model.
-   *
-   * It is possible to define the correct functions that you need
-   * and have them return the correct datatypes, as is shown below,
-   * but the problem comes when you cannot access the other fields
-   * of the class
-   */
-
-  private trait DataType {
-
-  }
 
   def or(f2: T => Boolean) =
     add(f2, _ && _)
