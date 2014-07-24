8576f18ccafb1a7f072eb150bd0de09228c6e687
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-07-24 13:45:30 +0600

    fixed aggregateAverage and aggregateAverageBy methods

--- src/main/scala/query/AggregateBuilder.scala
+++ src/main/scala/query/AggregateBuilder.scala
@@ -9,17 +9,6 @@ import main.scala.datatypes.LineListObject
 
 class AggregateBuilder(f: List[LineListObject] => List[ResultListObject]) {
   def this() = this(_.map (x => new ResultListObject(x)))
-
-  def add(f2: List[LineListObject] => List[ResultListObject]) =
-    new AggregateBuilder(x => (f(x) zip f2(x)).map
-      {case (result1, result2) => result1 mergeAverage result2})
-
-  def aggregateAverage(list: List[LineListObject]): ResultListObject = {
-    val functionApplied = f(list)
-   (functionApplied.tail fold functionApplied.head) ((x, y) => {
-      x mergeAverage y
-    } )
-  }
   
   def aggregateSum(list: List[LineListObject]): ResultListObject = {
     val functionApplied = f(list)
@@ -35,16 +24,10 @@ class AggregateBuilder(f: List[LineListObject] => List[ResultListObject]) {
   //Averages the data points
   //(listObject) => listObject.date for date separation
   def aggregateAverageBy[K](list: List[LineListObject], aggregateBy: LineListObject => K): List[ResultListObject] =
-    aggregate(list, aggregateBy, aggregateAverage)
+    aggregateSumBy(list, aggregateBy) map (_.averageBy)
   
   //aggregates by a given stat, summing the data points
   def aggregateSumBy[K](list: List[LineListObject], aggregateBy: LineListObject => K): List[ResultListObject] = 
-    aggregate(list, aggregateBy, aggregateSum)
+    (list groupBy (aggregateBy)).map { case (k, resultList) => aggregateSum(resultList)}.toList
 
-  //private aggreagtor that takes the method to aggregate each datapoint with
-  private def aggregate[K](list: List[LineListObject],
-                           aggregateBy: (LineListObject) => K,
-                            aggregator: (List[LineListObject] => ResultListObject)): List[ResultListObject] = {
-    (list groupBy (aggregateBy)).map { case (k, resultList) => aggregator(resultList)}.toList
-  }
 }
--- src/main/scala/query/ResultListObject.scala
+++ src/main/scala/query/ResultListObject.scala
@@ -10,15 +10,28 @@ import main.scala.datatypes.LineListObject
  * manipulation, but for a result of a AggregateBuilder,
  * where most of the data is missing anyways, it is
  * fine -- particularly for testing
+ *
+ * numberAggregated is for the averaging class -- used to average
+ *
  */
 
-class ResultListObject(val lineObject: LineListObject) {
-  def mergeAverage(other: ResultListObject) = {
-    new ResultListObject(lineObject mergeAverage other.lineObject)
-  }
-  
+class ResultListObject(val lineObject: LineListObject, numberAggregated: Int) {
+  //the constructor for most cases -- only 1 object has been combined
+  //to make this
+  def this(lineObject: LineListObject) = this(lineObject, 1)
+
+  //adds one to the number aggregated
   def mergeSum(other: ResultListObject) = {
-    new ResultListObject(lineObject mergeSum other.lineObject)
+    new ResultListObject(lineObject mergeSum other.lineObject, numberAggregated + 1)
   }
+
+  //averages by the number aggregated.
+  // very useful for taking the averages
+  def averageBy: ResultListObject =
+    averageBy(numberAggregated)
+
+  def averageBy(number: Int): ResultListObject =
+    new ResultListObject(lineObject averageBy number)
+
   override def toString = lineObject.toString
 }
