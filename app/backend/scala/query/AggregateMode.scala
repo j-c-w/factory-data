909c3c9d2e5744442c57d6a707875c463c5b4c76
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-07-27 21:55:45 +0600

    replaced Aggregator with a series of AggregateMode classes
    Done because although related, the functions in Aggregator did not really rely on each other. This makes implementation of the aggregator easier, and also means that the types can be more easily extended

--- app/backend/scala/query/AggregateMode.scala
+++ app/backend/scala/query/AggregateMode.scala
@@ -5,19 +5,51 @@ import backend.scala.datatypes.DataType
 /*
  * Created by Jackson Woodruff on 27/07/2014 
  *
- * This is a class to help the QueryBuilder determine
- * whether to sum or to average values in a type safe
- * object oriented and easily extensible way.
+ * These classes were originally put together into
+ * one big class called the aggregator. However,
+ * thier functions, although related, are all
+ * independant of one another, and this format
+ * provides easier passing of data.
  */
 
 abstract class AggregateMode {
-  def aggregateData[T](data: List[T]) = {
-    Aggregator.aggregateWith(data, this)
-  }
+  protected def converter[T <: DataType[T]](list: List[T]): List[ResultListObject[T]] =
+    list map (x => new ResultListObject[T](x))
+
+  def aggregate[T <: DataType[T]](data: List[T]): List[ResultListObject[T]]
 }
 
-case object AggregateSum extends AggregateMode
-case object AggregateAverage extends AggregateMode
-case object NoAggregate extends AggregateMode
-case class AggregateAverageBy[K, T <: DataType[T]](f: T => K)extends AggregateMode
-case class AggregateSumBy[K, T <: DataType[T]](f:  T => K) extends AggregateMode
+case object AggregateSum extends AggregateMode {
+  def aggregate[T <: DataType[T]](data: List[T]): List[ResultListObject[T]] = List({
+    val functionApplied = converter(data)
+    (functionApplied.tail fold functionApplied.head)((x, y) => {
+      x mergeSum y
+    })
+  })
+}
+case object AggregateAverage extends AggregateMode {
+  def aggregate[T <: DataType[T]](data: List[T]): List[ResultListObject[T]] = {
+    //doing a head operation w/out checking is fine because the
+    //aggregatesum is (at the moment) guaranteed to return a list with
+    //one item
+    List(AggregateSum.aggregate(data).head.averageBy)
+  }
+}
+case object NoAggregate extends AggregateMode {
+  def aggregate[T <: DataType[T]](data: List[T]): List[ResultListObject[T]] =
+    data map (x => new ResultListObject(x))
+}
+case class AggregateAverageBy[K, T <: DataType[T]](f: T => K)extends AggregateMode {
+  def aggregate[G <: T](data: List[G]): List[ResultListObject[G]] = {
+    (new AggregateSumBy(f).aggregate(data)) map (_.averageBy)
+  }
+
+}
+case class AggregateSumBy[K, T <: DataType[T]](f:  T => K) extends AggregateMode {
+  def aggregate[G <: T](data: List[G]): List[ResultListObject[G]] = {
+    val groupedDataSet = (data groupBy f) map { case(_, groupedData) => groupedData}
+    //once again, it is worth nothing that the .head call is perfectly safe --
+    //aggregateSum will always return a single item in a list
+    (groupedDataSet map (AggregateSum.aggregate(_).head)).toList
+  }
+}
--- app/backend/scala/query/QueryBuilder.scala
+++ app/backend/scala/query/QueryBuilder.scala
@@ -11,13 +11,13 @@ import backend.scala.datatypes.DataType
  */
 
 class QueryBuilder[T <: DataType[T]](filterBuilder: FilterBuilder[T],
-                                      aggregateBuilder: AggregateMode,
+                                      aggregateMode: AggregateMode,
                                       sortBuilder: SortBuilder[T]) {
-  def this() = this(null, NoAggregate, null)
+  def this() = this(null, NoAggregate[Nothing, Nothing], null)
 
   def addFilter(f: T => Boolean, combinator: (Boolean, Boolean) => Boolean) =
-    if (filterBuilder == null) new QueryBuilder(new FilterBuilder[T](f), aggregateBuilder, sortBuilder)
-    else new QueryBuilder(filterBuilder.add(f, combinator), aggregateBuilder, sortBuilder)
+    if (filterBuilder == null) new QueryBuilder(new FilterBuilder[T](f), aggregateMode, sortBuilder)
+    else new QueryBuilder(filterBuilder.add(f, combinator), aggregateMode, sortBuilder)
 
   def addFilterAnd(f: T => Boolean) =
     addFilter(f, _ && _)
@@ -26,13 +26,13 @@ class QueryBuilder[T <: DataType[T]](filterBuilder: FilterBuilder[T],
     addFilter(f, _ || _)
 
   def setAggregate(mode: AggregateMode) =
-    new QueryBuilder(filterBuilder, mode, sortBuilder)
+    new QueryBuilder[T](filterBuilder, mode, sortBuilder)
 
   def addSort(f: (ResultListObject[T], ResultListObject[T]) => Boolean) =
-    if (sortBuilder == null) new QueryBuilder(filterBuilder, aggregateBuilder, new SortBuilder[T](f, true))
-    else new QueryBuilder(filterBuilder, aggregateBuilder, sortBuilder.add(f))
+    if (sortBuilder == null) new QueryBuilder(filterBuilder, aggregateMode, new SortBuilder[T](f, true))
+    else new QueryBuilder(filterBuilder, aggregateMode, sortBuilder.add(f))
 
-  def processData(data: List[T], aggregateMode: AggregateMode) = {
+  def processData(data: List[T]) = {
     sortData(aggregateData(filterData(data)))
   }
 
@@ -41,7 +41,7 @@ class QueryBuilder[T <: DataType[T]](filterBuilder: FilterBuilder[T],
     else filterBuilder.filter(data)
 
   private def aggregateData(data: List[T]): List[ResultListObject[T]] =
-    Aggregator.aggregateWith(data, aggregateBuilder)
+    aggregateMode.aggregate(data)
 
   private def sortData(data: List[ResultListObject[T]]) = {
     if (sortBuilder == null) data
