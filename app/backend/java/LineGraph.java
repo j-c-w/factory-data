7334e9d4ded41c0873d1a92dbb55cd10e5cd9e77
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-07-31 11:58:25 -0700

    Fixed line graph to shrink labels when nessecary

--- app/backend/java/LineGraph.java
+++ app/backend/java/LineGraph.java
@@ -6,11 +6,28 @@ package backend.java;
  */
 
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class LineGraph extends Graph {
     public LineGraph(DefaultCategoryDataset dataset, String title, String xAxisTitle, String yAxisTitle) {
-        chart = ChartFactory.createLineChart(title, xAxisTitle, yAxisTitle, dataset, PlotOrientation.VERTICAL, true, true, false);
+        chart = createChart(dataset, title, xAxisTitle, yAxisTitle);
     }
+
+    public JFreeChart createChart(DefaultCategoryDataset dataset, String title, String xAxisTitle, String yAxisTitle) {
+        CategoryPlot categoryPlot = toCategoryPlot(dataset);
+        //this sets the data (and only the data) to the chart.
+        // in this line, categoryPlot.getDataset could well be replaced by
+        //just dataset (the parameter)
+        JFreeChart chart = ChartFactory.createLineChart(title, yAxisTitle, xAxisTitle, categoryPlot.getDataset(), PlotOrientation.VERTICAL, true, false, false);
+        //this line sets the x axis to the exact x axis from the
+        //category plot (which fits the values to size)
+        chart.getCategoryPlot().setDomainAxis(categoryPlot.getDomainAxis());
+        chart.setTitle(title);
+        return chart;
+    }
+
+
 }
