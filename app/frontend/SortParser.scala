03a127192ca645fa46a10ed97c32e1a1c91e5cd2
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-08-26 22:26:43 +0100

    added SortParser class

---
+++ app/frontend/SortParser.scala
@@ -0,0 +1,26 @@
+package frontend
+
+import backend.scala.datatypes.{LineListObject, DataField}
+import backend.scala.datatypes.options.MathComparable
+import backend.scala.query.SortBuilder
+
+/*
+ * Created by Jackson Woodruff on 25/08/2014 
+ *
+ * After already having designed two classes,
+ * I have decided that there is nothing worth
+ * keeping about a class for these parser
+ * things.
+ */
+
+object SortParser {
+  def toList =
+    List("Ascending", "Descending")
+
+  def fromString[T <: MathComparable[T]](string: String, field: DataField[T], comparator: ComparisonMethod) = string match {
+    case "Ascending" => new SortBuilder[LineListObject](
+      (o1, o2) => comparator.compare(field.get(o1.lineObject), field.get(o2.lineObject)), true)
+    case ("Descending") => new SortBuilder[LineListObject](
+      (o1, o2) => !comparator.compare(field.get(o1.lineObject), field.get(o2.lineObject)), true)
+  }
+}
