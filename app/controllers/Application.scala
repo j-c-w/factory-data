5ae690f9a7750d59bf63345904ae65604ea06782
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-09-19 22:20:56 +0100

    updated routes & Application with new submitForm route
    This is, needless to say, for submitting forms

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,18 +5,23 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+  private val tableHeaders: List[String] = List(
+    "Factory Code", "Line Code", "Date", "Total Operators",
+    "Operators Present", "Operators Absent", "Operators on Leave",
+    "Percent Operators Absent", "Total Helpers", "Helpers Present",
+    "Helpers Absent", "Helpers on Leave", "Percent Helpers Absent"
+  )
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def submitForm = Action {
+    Ok(views.html.dataView(Backend.loadRaw, tableHeaders))
+  }
+
   def list = Action {
-    Ok(views.html.dataView(Backend.loadRaw, List(
-      "Factory Code", "Line Code", "Date", "Total Operators",
-      "Operators Present", "Operators Absent", "Operators on Leave",
-      "Percent Operators Absent", "Total Helpers", "Helpers Present",
-      "Helpers Absent", "Helpers on Leave", "Percent Helpers Absent"
-    )))
+    Ok(views.html.dataView(Backend.loadRaw, tableHeaders))
   }
 
 }
