50b4bc05c464322a013ff58763026c64a5dad784
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-09-30 10:36:00 +0100

    Split up a rather large DataManipulationForm file into many smaller files

---
+++ app/frontend/forms/FormData.scala
@@ -0,0 +1,13 @@
+package frontend.forms
+
+/*
+ * Created by Jackson Woodruff on 30/09/2014
+ *
+ * A simple trait to enable the passing
+ * of any of these to a single parser method,
+ * enabling me to store these in a single list
+ */
+
+trait FormData[T <: FormData[T]] {
+  def default: T
+}
---
+++ app/frontend/forms/SortFormParser.scala
@@ -0,0 +1,43 @@
+package frontend.forms
+
+/*
+ * Created by Jackson Woodruff on 30/09/2014
+ *
+ * This is a class for holding the data entered in the form
+ * At some point, the parameters that are taken
+ * should be converted to lists to make multiple queries easier.
+ */
+case class SearchFormParser(filterData: FilterFormData, aggregateData: AggregateFormData, sortData: SortFormData)
+              extends FormData[SearchFormParser] {
+  def this() = this(new FilterFormData, new AggregateFormData, new SortFormData)
+
+  def default = new SearchFormParser
+}
+
+
+
+/*
+ * This is a set of three classes that
+ * take the data. Will potentially modify
+ * so they can return the correct ...Parser
+ * classes.
+ */
+protected case class SortFormData(searchField: String, sortMethod: String) extends FormData[SortFormData] {
+  def this() = this("", "")
+
+  def default = new SortFormData
+}
+
+case class FilterFormData(filteringField: String, filterComparator: String, filterText: String)
+              extends FormData[FilterFormData] {
+  def this() = this("", "", "")
+
+  def default = new FilterFormData
+}
+
+protected case class AggregateFormData(aggregatingField: String, aggregateMode: String)
+              extends FormData[AggregateFormData] {
+  def this() = this("", "")
+
+  def default = new AggregateFormData
+}
