ddaafd6a715f9a176956dc63fe72f7f39dacd721
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-09-30 11:04:31 +0100

    changed FilterParser from a class to an Object

--- app/frontend/FilterParser.scala
+++ app/frontend/FilterParser.scala
@@ -7,13 +7,9 @@ import backend.scala.query.FilterBuilder
 /*
  * Created by Jackson Woodruff on 24/08/2014 
  *
- * This could be an object right now.
- *
- * However, to facilitate potential design additions,
- * I have left this as a class.
  */
 
-class FilterParser[A <: MathComparable[A]](field: DataField[A], comparison: ComparisonMethod, value: A) {
-  def getBuilder: FilterBuilder[LineListObject] =
+object FilterParser {
+  def getBuilder[A <: MathComparable[A]](field: DataField[A], comparison: ComparisonMethod, value: A): FilterBuilder[LineListObject] =
     new FilterBuilder[LineListObject](x => comparison.compare(value, field.get(x)))
 }
