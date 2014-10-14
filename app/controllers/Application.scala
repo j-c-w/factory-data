9b137bd3698ed88f29a2bc56ade69562d9e164aa
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-10-14 10:38:27 +0100

    changed the submitForm method to use the new FormToQuery object

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,10 @@
 package controllers
 
 import backend.scala.Backend
-import frontend.forms.{SearchFormParser, DataManipulationForm}
+import backend.scala.datatypes.{DataField, OperatorsAbsent, LineListObject}
+import backend.scala.query.{FilterBuilder, QueryBuilder, NoAggregate}
+import frontend.{FilterParser, ComparisonMethod, Equals}
+import frontend.forms.{FormToQuery, FilterFormData, SearchFormParser, DataManipulationForm}
 import play.api._
 import play.api.data.{Form, Field}
 import play.api.mvc._
@@ -9,7 +12,7 @@ import play.api.mvc._
 import scala.util.{Success, Failure, Try}
 
 object Application extends Controller {
-  private val tableHeaders: List[String] = List(
+  val tableHeaders: List[String] = List(
     "Factory Code", "Line Code", "Date", "Total Operators",
     "Operators Present", "Operators Absent", "Operators on Leave",
     "Percent Operators Absent", "Total Helpers", "Helpers Present",
@@ -20,14 +23,22 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def submitForm = Action { /*implicit request =>
-    val filledFormTry = Try(DataManipulationForm.form.bindFromRequest.get)
-    val filledForm = filledFormTry match {
-      case Failure(fail) => new SearchFormParser
-      case Success(parser) => parser
-    }*/
-    Ok(views.html.statsTestView(Backend.loadRaw))
-    //Ok(views.html.dataView(Backend.loadRaw, tableHeaders, Form(DataManipulationForm.filterForm)))
+  def submitForm = Action { implicit request =>
+    //this first gets the form from the request
+    val filledFormTry = Try(
+      DataManipulationForm.form.bindFromRequest.get)
+
+    //this then takes the form out of the monad
+    //and if there was a problem resets it
+    val submittedForm: SearchFormParser = filledFormTry match {
+      case Success(form) => form
+      case Failure(failureMessage) => new SearchFormParser
+    }
+
+    //finally, this creates the query using the FormToQuery calss
+    val query = FormToQuery.wholeForm(submittedForm)
+    //and then this pushes it to the view -- success!!
+    Ok(views.html.dataView(query.processData(Global.baseData).toArray, tableHeaders, Form(DataManipulationForm.filterForm)))
   }
 
   def list = Action {
