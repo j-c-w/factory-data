16031ab1d7a79664ceadc65e6b3ed46ae99b3a74
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-11-13 15:53:11 +0000

    data now does lazy-loading

---
+++ public/javascripts/lazy-list.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jackson on 11/11/2014.
+ */
+$(document).ready(function() {
+    $(".lazyList").jscroll({
+        padding: 20,
+        nextSelector: 'a.jscroll-next:last',
+        loadingHtml: '<img src="/assets/images/loading.gif" alt="Loading" />' // The HTML to show at the bottom of the content while loading the next set.
+    });
+});
