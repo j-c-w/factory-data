746b59eeeff3442956bd938f37e4572e634672f5
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-11-13 16:05:01 +0000

    the whole table now scrolls horizontally but only the data scrolls vertically

--- public/javascripts/lazy-list.js
+++ public/javascripts/lazy-list.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function() {
     $(".lazyList").jscroll({
-        padding: 20,
+        padding: 60,
         nextSelector: 'a.jscroll-next:last',
         loadingHtml: '<img src="/assets/images/loading.gif" alt="Loading" />' // The HTML to show at the bottom of the content while loading the next set.
     });
