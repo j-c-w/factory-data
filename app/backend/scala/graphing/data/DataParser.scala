d33342a6b56b5ff9eb87ce55836dd1be8ab2e8f3
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-11-16 12:51:22 +0000

    datasets are now sorted by their xAxis values

--- app/backend/scala/graphing/data/DataParser.scala
+++ app/backend/scala/graphing/data/DataParser.scala
@@ -1,7 +1,9 @@
 package backend.scala.graphing.data
 
 import backend.scala.datatypes.DataType
+import backend.scala.datatypes.options.MathComparable
 import backend.scala.query.ResultListObject
+import java.lang.Comparable
 
 /*
  * Created by Jackson Woodruff on 26/07/2014 
@@ -11,12 +13,19 @@ import backend.scala.query.ResultListObject
  * the two on request.
  */
 
-class DataParser[A, T <: DataType[T]](data: List[ResultListObject[T]],
+class DataParser[A <: Comparable[_], T <: DataType[T]](data: List[ResultListObject[T]],
                                       converter: ResultListObject[T] => (A, Double),
                                       series: String) {
   lazy val parse: XYData[A, Double] = {
-    val zippedData  = (data map converter)
-    val (x, y) = zippedData.unzip
+    val zippedData  = (data map converter).sortWith {
+      //we have to sort with strings because the compiler is dying on me
+      // and I do not have the time/stamina to sort out all the type errors
+      //without an in-ide type checker.
+      //Please do update this if possible
+      case ((xAxis, _), (xAxisTwo, _)) => xAxis.toString().compareTo(xAxisTwo.toString()) < 1
+    }
+    println("data sorted")
+    val (x, y) = zippedData.toList.unzip
     new XYData[A, Double](x, y, series)
   }
 }
