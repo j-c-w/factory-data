787f8f1f18caec4b9a3fe7a649a0f73b91557471
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-11-25 18:54:09 +0000

    updated dataloader so it now loads from the new harmonized dataset

--- app/backend/scala/DataLoader.scala
+++ app/backend/scala/DataLoader.scala
@@ -1,7 +1,7 @@
 package backend.scala
 
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{Locale, Date}
 import backend.scala.datatypes.options._
 import backend.scala.datatypes._
 import main.backend.csv.scala.com.github.tototoshi.csv.CSVReader
@@ -33,28 +33,25 @@ object DataLoader {
 
   def rowToObject(list: List[String]): LineListObject = new LineListObject(
     getDate(list(2)),
-    new LineData(IntegerOption.toIntegerOptionOrNone(list(3)), IntegerOption.toIntegerOptionOrNone(list(4)), IntegerOption.toIntegerOptionOrNone(list(5)),
-      IntegerOption.toIntegerOptionOrNone(list(6)), IntegerOption.toIntegerOptionOrNone(list(7)), IntegerOption.toIntegerOptionOrNone(list(8))),
-    new OrderData(SomeString(list(9)), SomeString(list(10)), IntegerOption.toIntegerOptionOrNone(list(11)), DoubleOption.toDoubleOptionOrNone(list(12)),
-      SomeString(list(13)), DoubleOption.toDoubleOptionOrNone(list(14)), DoubleOption.toDoubleOptionOrNone(list(15)), IntegerOption.toIntegerOptionOrNone(list(16))),
-    new TargetData(DoubleOption.toDoubleOptionOrNone(list(17)), DoubleOption.toDoubleOptionOrNone(list(18)), DoubleOption.toDoubleOptionOrNone(list(19)),
-      DoubleOption.toDoubleOptionOrNone(list(20)), DoubleOption.toDoubleOptionOrNone(list(21))),
-    new IOData(DoubleOption.toDoubleOptionOrNone(list(22)), DoubleOption.toDoubleOptionOrNone(list(23)), DoubleOption.toDoubleOptionOrNone(list(24)),
-      DoubleOption.toDoubleOptionOrNone(list(25)), DoubleOption.toDoubleOptionOrNone(list(26)), DoubleOption.toDoubleOptionOrNone(list(27))),
-    new QualityData(DoubleOption.toDoubleOptionOrNone(list(28)), DoubleOption.toDoubleOptionOrNone(list(29)), DoubleOption.toDoubleOptionOrNone(list(30)),
-      DoubleOption.toDoubleOptionOrNone(list(31)), DoubleOption.toDoubleOptionOrNone(list(32)), DoubleOption.toDoubleOptionOrNone(list(33))),
-    new AbsenteeismData(DoubleOption.toDoubleOptionOrNone(list(34)), DoubleOption.toDoubleOptionOrNone(list(35)), DoubleOption.toDoubleOptionOrNone(list(36)),
-      DoubleOption.toDoubleOptionOrNone(list(37)), DoubleOption.toDoubleOptionOrNone(list(38)), DoubleOption.toDoubleOptionOrNone(list(39)),
-      DoubleOption.toDoubleOptionOrNone(list(40)), DoubleOption.toDoubleOptionOrNone(list(41)), DoubleOption.toDoubleOptionOrNone(list(42)),
-      DoubleOption.toDoubleOptionOrNone(list(43))),
-    IntegerOption(1), IntegerOption.toIntegerOptionOrNone(list(1))
+    new LineData(IntegerOption.toIntegerOptionOrNone(list(2)), IntegerOption.toIntegerOptionOrNone(list(3)), IntegerOption.toIntegerOptionOrNone(list(4)),
+      IntegerOption.toIntegerOptionOrNone(list(5)), IntegerOption.toIntegerOptionOrNone(list(6))),
+    new OrderData(IntegerOption.toIntegerOptionOrNone(list(7)), DoubleOption.toDoubleOptionOrNone(list(8)), DoubleOption.toDoubleOptionOrNone(list(9))),
+    new TargetData(DoubleOption.toDoubleOptionOrNone(list(10)), DoubleOption.toDoubleOptionOrNone(list(11)), DoubleOption.toDoubleOptionOrNone(list(12))),
+    new IOData(DoubleOption.toDoubleOptionOrNone(list(13)), DoubleOption.toDoubleOptionOrNone(list(14)), DoubleOption.toDoubleOptionOrNone(list(15)),
+      DoubleOption.toDoubleOptionOrNone(list(16)), DoubleOption.toDoubleOptionOrNone(list(17))),
+    new QualityData(DoubleOption.toDoubleOptionOrNone(list(18)), DoubleOption.toDoubleOptionOrNone(list(19)), DoubleOption.toDoubleOptionOrNone(list(25))),
+    new AbsenteeismData(DoubleOption.toDoubleOptionOrNone(list(20)), DoubleOption.toDoubleOptionOrNone(list(21)), DoubleOption.toDoubleOptionOrNone(list(22)),
+      DoubleOption.toDoubleOptionOrNone(list(23)), DoubleOption.toDoubleOptionOrNone(list(24))),
+    IntegerOption(1), IntegerOption.toIntegerOptionOrNone(list(0))
   )
 
   private def getDate(date: String) = {
-    val split = date.split("-")
-    Try(new FactoryDate(IntegerOption.toIntegerOptionOrNone(split(2)),
-      IntegerOption.toIntegerOptionOrNone(split(1)),
-      IntegerOption.toIntegerOptionOrNone(split(0)))).getOrElse(
+    val dateFormat = new SimpleDateFormat("MMM", Locale.ENGLISH).parse(date)
+
+
+    Try(new FactoryDate(IntegerOption(dateFormat.getDay),
+      IntegerOption(dateFormat.getMonth),
+      IntegerOption(dateFormat.getYear))).getOrElse(
         new FactoryDate(NoInteger, NoInteger, NoInteger)
       )
   }
