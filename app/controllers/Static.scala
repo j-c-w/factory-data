beb2ebc745b1bb8b39833c25b32db2d76e7eb233
Jackson Woodruff <woodruff.jackson@gmail.com>
2015-01-11 13:59:03 +0000

    change strings in comparison method to plain english rather than symbols
    i.e. == -> equal to

--- app/controllers/Static.scala
+++ app/controllers/Static.scala
@@ -2,6 +2,7 @@ package controllers
 
 import backend.scala.datatypes._
 import backend.scala.graphing.regressions.RegressionGenerator
+import frontend.ComparisonMethod
 
 /*
  * Created by Jackson Woodruff on 13/11/2014 
@@ -33,9 +34,8 @@ object Static {
 
   val numberHeaders: List[String] = noSelection :: DataField.asDoublesList
 
-  val comparisonMethods: List[String] = List(
-    "==", "!=", "<=", ">=", ">", "<"
-  )
+  val comparisonMethods: List[String] =
+    ComparisonMethod.asList
 
   val graphSortOptions: List[String] = List(
     "xAxis Ascending", "xAxis Descending", "No Sort", "yAxis Ascending", "yAxis Descending"
