687745cba6d2c5378fc7b0758647b33831ad2493
Jackson Woodruff <woodruff.jackson@gmail.com>
2015-03-27 15:14:36 +0000

    changed the headers of the help tuples

--- app/controllers/Static.scala
+++ app/controllers/Static.scala
@@ -60,10 +60,10 @@ object Static {
   val regressions: List[String] = noSelection :: RegressionGenerator.toList
 
   object Assets {
-    val filterHelp = ("Filter Help", "A description of help")
-    val aggregateHelp = ("Aggregate Help", "Help")
-    val sortHelp = ("Sort Help", "help")
-    val graphHelp = ("Graph Help", "help")
-    val columnHelp = ("Column Help", "help")
+    val filterHelp = ("Filter", "A description of help")
+    val aggregateHelp = ("Aggregate", "Help")
+    val sortHelp = ("Sort", "help")
+    val graphHelp = ("Graph", "help")
+    val columnHelp = ("Column", "help")
   }
 }
