002cb431ae91b70625219e85fc90d09970ba78d3
Jackson Woodruff <woodruff.jackson@gmail.com>
2015-04-07 12:59:29 +0100

    changed the home page layout to make room for a scrolling graph gallery

---
+++ public/javascripts/main-menu.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jackson on 4/6/2015.
+ */
+
+
+/*
+ * This sets up the scrolling of the graphs on the main page.
+ *
+ * An explanation of the settings can be seen here:
+ *      http://marktyrrell.com/labs/blueberry/
+ */
+$(document).ready(function(){
+    $(".blueberry").blueberry({
+        interval: 4000
+    });
+});
